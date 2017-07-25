1dea9e2317076a99f7533bc3265ebe6d2394ada2
brant <sjwu1234@gmail.com>
2017-07-25 16:59:16 +0800

    Initial Commit

---
+++ YTMediatorDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YTMediatorDemo
+//
+//  Created by brant on 2017/7/25.
+//  Copyright © 2017年 lhtc-mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YTMediatorDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YTMediatorDemo
+//
+//  Created by brant on 2017/7/25.
+//  Copyright © 2017年 lhtc-mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
